8f38bbf6479509713d8e83b148f28c7df4c98cd1
habeola <habeolar89@gmai.com>
2020-02-04 16:11:11 +0100

    A blog

---
+++ src/blog/urls.py
@@ -0,0 +1,40 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from post.views import index, blog, post, search, create, update, delete
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog, name='blog-list'),
+    path('search/', search, name='search'),
+    path('create/', create, name='post-create'),
+    path('post/<id>/', post, name='post-detail'),
+    path('post/<id>/update/', update, name='post-update'),
+    path('post/<id>/delete/', delete, name='post-delete'),
+    path('tinymce/', include('tinymce.urls')),
+    path('accounts/', include('allauth.urls'))
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
